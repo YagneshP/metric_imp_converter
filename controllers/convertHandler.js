b741bdcb5a4b33572d355bd0dcaba035787354db
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-10 00:57:45 -0400

    fix: change the logic of get number fn by seprating from unit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,10 +1,25 @@
 function ConvertHandler() {
   const units = ["lbs", "km","gal","mi"];
   this.getNum = function(input) {
-		let numberRegex = /^(\d{1,}((\.)?(\d))?)((\/)\d{1,}((\.)?(\d))?)?/g  /*^(\d{1,}((.|\/)(\d{1,}))?)*/
-		input.toLowerCase();
-    let result = eval(input.match(numberRegex)[0]); // replace eval 
-    return result;
+		// let numberRegex = /^(\d{1,}((\.)?(\d))?)((\/)\d{1,}((\.)?(\d))?)?/g  /*^(\d{1,}((.|\/)(\d{1,}))?)*/
+		let consucetiveDotsRegex = /\..?(?=\.)/
+		let doubleDividerRegex = /\/.?(?=\/)/
+		let strRegex = /[a-z]/ // this regex will find the first word and its index in form of array
+		let inputLower =	input.toLowerCase(); 
+		let index = inputLower.match(strRegex)["index"];
+		let num;
+		if(index && index !== 0){
+			num = inputLower.slice(0,index);
+			if(!consucetiveDotsRegex.test(num)&&!doubleDividerRegex.test(num)){
+				num = eval(num); // replace eval 
+			}else{
+				throw Error("Invalid Number") // not sure should return error
+			}
+		}else{
+			num = 1
+		}
+    
+    return num;
   };
   
   this.getUnit = function(input) {
