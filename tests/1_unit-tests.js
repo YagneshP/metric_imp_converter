5cd9e5d44bf6861a01b059e257f74d79446ae18e
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-10 01:13:24 -0400

    test: add test for getNum fn return default 1 if no numeric input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -18,7 +18,10 @@ suite('Unit Tests', function(){
 		assert.strictEqual(convertHandler.getNum("4.1/3.5L"),4.1/3.5,"getting fractional with decimal number")
 	})
 	test("throw error when input has double-fraction",function(){
-		assert.throws(convertHandler.getNum("1/2/3mi"),"Invalid Number")
+		assert.throws(convertHandler.getNum("1/2/3mi"), errorInstance,"Invalid Number")
+	})
+	test("should return default numeric value 1 when no input numeric", function(){
+		assert.strictEqual(convertHandler.getNum("mi"),1,"getting 1 as default value for no input numeric");
 	})
 
 });
