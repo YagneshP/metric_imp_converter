426c24d06c98adb40c31524f9a0d22638ea390f0
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-12 19:31:40 -0400

    test: add GET /api/convert route test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,23 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+	suite('GET /api/convert', function () {
+		test("1.2mi", function (done) {
+			chai
+				.request(server)
+				.get("/api/convert")
+				.send({"initNum":1.2,"initUnit":'mi',"returnNum":1.93121,"returnUnit":'km',"string":'1.2 miles converts to 1.93121 kilometers'})
+				.end(function (err, res) {
+					if(err)	done(err)
+					assert.equal(res.status, 200, "response status should be 200");
+					assert.equal(res.body.initNum,1.2,"initNum is 1.2");
+					assert.equal(res.body.initUnit,'mi',"initUnit is mi");
+					assert.equal(res.body.returnNum,1.93121,"returnNum is 1.93121");
+					assert.equal(res.body.returnUnit,'km',"returnUnit is km");
+					assert.equal(res.body.string,'1.2 miles converts to 1.93121 kilometers','string should be in this format : {initNum} {initUnitString} converts to {returnNum} {returnUnitString}')
+					// assert.property(req.body,'string');
+					done();
+				});
+		});
+});
 });
