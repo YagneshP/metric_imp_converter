1e64500df1948ec27e475001a0b656dcd3071222
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-04-12 15:16:52 -0400

    feat: add GET route for api/convert and return json

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,14 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+	app.route("/api/convert")
+		 .get(function(req,res){
+			 let input = req.body
+			 let initNum = convertHandler.getNum(input);
+			 let initUnit = convertHandler.getUnit(input);
+			 let returnUnit = convertHandler.getReturnUnit(initUnit);
+			 let returnNum = convertHandler.convert(initNum,initUnit);
+			 let string = convertHandler.getString(initNum,initUnit,returnNum,returnUnit);
+			 return res.json({"initNum":initNum,"initUnit":initUnit,"returnNum":returnNum,"returnUnit":returnUnit,"string":string})
+		 })
 };
